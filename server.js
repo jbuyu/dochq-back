780d791163f6dbd5de727e3e7d23832814bdd97e
jbuyu <jbuyu@ymail.com>
2020-08-10 19:45:07 +0300

    changes mail service provider to sendgrid

--- server.js
+++ server.js
@@ -2,25 +2,16 @@ const dotenv = require("dotenv");
 dotenv.config();
 const cors = require("cors");
 const express = require("express");
-const bodyParser = require("body-parser");
 const PORT = process.env.PORT || 3000;
-const nodemailer = require("nodemailer");
-
+const sgMail = require("@sendgrid/mail");
+sgMail.setApiKey(process.env.MAIL_KEY);
 const app = express();
 //middleware
 app.use(cors());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
-//nodemailer
-const contactAddress = "staunchwriter@gmail.com";
-const mailer = nodemailer.createTransport({
-  service: "Gmail",
-  auth: {
-    user: process.env.GMAIL_ADDRESS,
-    pass: process.env.PASSWORD,
-  },
-});
+//sengrid
 app.get("/", (req, res) => {
   res.json({
     message: "doc-api",
@@ -30,28 +21,34 @@ app.get("/", (req, res) => {
 app.post("/form", (req, res) => {
   const { newClientData } = req.body;
   console.log("new", newClientData);
-  mailer.sendMail(
-    {
-      from: "saucewriter@gmail.com",
-      to: [contactAddress],
-      subject: `New Online Patient - ${newClientData.name}`,
+  sgMail
+    .send({
+      from: process.env.EMAIL_FROM,
+      to: process.env.EMAIL_TO,
+      subject: "Account activation link",
       html: `
-                <h3>Phone: ${newClientData.phone}</h3>
-                <h3>Age: ${newClientData.age}yrs</h3>
-                <h4>Consultation: ${newClientData.consultation}</h4>
-                <h4>Symptoms: ${newClientData.symptoms}</h4>
-                <h4>Gender: ${newClientData.gender}</h4>
-                <h4>Email: ${newClientData.email}</h4>
-                <hr />
-                <p>This email may containe sensetive information</p>
-            `,
-    },
-    function (err) {
-      if (err) return res.status(500).send(err);
-      // res.json({ success: true });
-      console.log("sent");
-    }
-  );
+        <h3>Phone: ${newClientData.phone}</h3>
+        <h3>Age: ${newClientData.age}yrs</h3>
+        <h4>Consultation: ${newClientData.consultation}</h4>
+        <h4>Symptoms: ${newClientData.symptoms}</h4>
+        <h4>Gender: ${newClientData.gender}</h4>
+        <h4>Email: ${newClientData.email}</h4>
+        <hr />
+        <p>This email may contain sensetive information</p>
+`,
+    })
+    .then((sent) => {
+      return res.json({
+        message: `Email has been sent to ${process.env.EMAIL_TO}`,
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      return res.status(400).json({
+        success: false,
+        errors: err,
+      });
+    });
 });
 app.listen(PORT, () => {
   console.log("listening on port,", PORT);
