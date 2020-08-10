d567c7f3339a5fc6d87f0b167bfbf279f9fe3d5c
jbuyu <jbuyu@ymail.com>
2020-08-10 14:21:33 +0300

    misc settings and fixes

--- server.js
+++ server.js
@@ -1,35 +1,43 @@
+const dotenv = require("dotenv");
+dotenv.config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const PORT = process.env.PORT || 3000;
 const nodemailer = require("nodemailer");
 
 const app = express();
-app.use(bodyParser.urlencoded());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-const contactAdress = "ajusabuyu@gmail.com";
+const contactAddress = "staunchwriter@gmail.com";
 
 const mailer = nodemailer.createTestAccount({
   service: "Gmail",
   auth: {
-    user: process.env.production.GMAIL_ADDRESS,
-    pass: process.env.production.PASSWORD,
+    user: process.env.GMAIL_ADDRESS,
+    pass: process.env.PASSWORD,
   },
 });
 
-app.post("/form", (req, res) => {
-  mailer.sendMail(
-    {
-      from: "ajusabuyu@gmail.com",
-      to: [contactAddress],
-      subject: req.body.subject || "[No subject]",
-      html: req.body.message || "[No message]",
-    },
-    function (err, info) {
-      if (err) return res.status(500).send(err);
-      res.json({ success: true });
-    }
-  );
+app.post("/form", async (req, res) => {
+  try {
+    mailer.sendMail(
+      {
+        from: "ajusabuyu@gmail.com",
+        to: [contactAddress],
+        subject: req.body.subject || "[New Patient]",
+        html: req.body.message || "[No message]",
+      },
+      function (err) {
+        if (err) return res.status(500).send(err);
+        res.json({ success: true });
+      }
+    );
+  } catch (err) {
+    next(err);
+  }
 });
+console.log(process.env.PASSWORD);
 app.listen(PORT, () => {
   console.log("listening on port,", PORT);
 });
